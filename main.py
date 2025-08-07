8de4ef1b0dac1b5fbf1aac46195dde99ace9430e
Wardenbarde <J.l.feutner@gmail.com>
2025-08-07 18:33:09 +0200

    build the funktions: txt to string, to count the words and main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    booktxt = get_book_text("./books/frankenstein.txt")
+    num_words = count_book_words(booktxt)
+    txt = f"{num_words} words found in the document"
+    print(txt)
+    return 0
+
+def count_book_words(booktext):
+    nummber = len(booktext.split())
+    return nummber
+
+main()
+
