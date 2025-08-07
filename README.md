6c7b1629a667bc061a8be92798aca348f16ea81c
Wardenbarde <J.l.feutner@gmail.com>
2025-08-07 18:38:27 +0200

    commenting my work

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # bookbot# bookbot
 
 BookBot is my first [Boot.dev](https://www.boot.dev) project!
+
+07.08.25 schreiben der funktionen für das wandeln der txt datei in einen sting und zum zählen der wörter
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+
+#Funktion to grap a text and mak it to a string: gives string back
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-
+#main Funktion to give a count of words in a book
 def main():
     booktxt = get_book_text("./books/frankenstein.txt")
     num_words = count_book_words(booktxt)
     txt = f"{num_words} words found in the document"
     print(txt)
     return 0
-
+# funktion to count the words form the string
 def count_book_words(booktext):
     nummber = len(booktext.split())
     return nummber
